5a28af5235ee29e126c29f1eb68b092e40bfb809
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-05-02 16:07:23 -0500

    Added MediaControlPage with dummy volume slider.

---
+++ HomeScreenApp/MediaControlPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HomeScreenApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MediaControlPage : Page
+    {
+        Uri deviceUri = null;
+
+        public MediaControlPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            deviceUri = e.Parameter as Uri;
+            if (deviceUri != null) {
+                Debug.WriteLine("Device URI: " + deviceUri.ToString());
+            }
+            else {
+                Debug.WriteLine("Device URI is null!");
+            }
+        }
+    }
+}
--- HomeScreenApp/MusicDeviceSelection.xaml.cs
+++ HomeScreenApp/MusicDeviceSelection.xaml.cs
@@ -27,33 +27,61 @@ namespace HomeScreenApp
     /// </summary>
     public sealed partial class MusicDeviceSelection : Page
     {
+        int chromecastCounter;
+
         public MusicDeviceSelection()
         {
             this.InitializeComponent();
             this.spinner.IsActive = true;
+            this.chromecastCounter = 0;
             loadDevicesAsync();
         }
 
         public async void loadDevicesAsync()
         {
+            this.chromecastCounter = 0;
+            List<ChromecastItem> items = new List<ChromecastItem>();
+
             Debug.WriteLine("Searching for cast devices...");
             ObservableCollection<Chromecast> chromecasts = await ChromecastService.Current.StartLocatingDevices("192.168.0.108");
             Debug.WriteLine("Found " + chromecasts.Count + " elements");
 
-            List<ChromecastItem> sourceList = new List<ChromecastItem>();
 
             foreach (Chromecast chromecast in chromecasts) {
-                sourceList.Add(new ChromecastItem() { DeviceName = chromecast.FriendlyName });
+                ChromecastItem item = new ChromecastItem()
+                {
+                    DeviceName = chromecast.FriendlyName,
+                    DeviceUri = chromecast.DeviceUri,
+                    DeviceId = "dev" + this.chromecastCounter++,
+                    Device = chromecast
+                };
+
+                items.Add(item);
                 Debug.WriteLine("Added cast device " + chromecast.FriendlyName);
+                Debug.WriteLine("\tDevice URI: " + chromecast.DeviceUri);
+                Debug.WriteLine("\tDevice ID: " + item.DeviceId);
+                
             }
 
-            this.deviceList.ItemsSource = sourceList;
+            this.deviceList.ItemsSource = items;
             this.spinner.IsActive = false;
+        }
+        
+        private void deviceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ChromecastItem item = (ChromecastItem)this.deviceList.SelectedItem;
+            Debug.WriteLine("Device URI to send: " + item.DeviceUri);
+            Debug.WriteLine("\tFriendly name: " + item.DeviceName);
+
+            this.Frame.Navigate(typeof(MediaControlPage), item.DeviceUri);
         }
     }
 
     public class ChromecastItem
     {
         public string DeviceName { get; set; }
+        public Uri DeviceUri { get; set; }
+        public string DeviceId { get; set; }
+        public Chromecast Device { get; set; }
     }
 }
